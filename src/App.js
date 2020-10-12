9fa655e6bbaa01a64016edf31c624456388e9415
maitamin <maitamin@gmail.com>
2020-10-12 23:17:39 +0900

    モデル導入

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 console.log("App.js: loaded");
 
-import { element } from "./view/html-util.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { element, render } from "./view/html-util.js";
 
 export class App {
     constructor() {
-        console.log("App initialized");
+        // console.log("App initialized");
+        this.todoListModel = new TodoListModel();
     }
 
     mount() {
-        const fromElement = document.querySelector("#js-form");
+        const formElement = document.querySelector("#js-form");
         const inputElement = document.querySelector("#js-form-input");
         const containerElement = document.querySelector("#js-todo-list");
         const todoItemCountElement = document.querySelector("#js-todo-count");
         
-        let todoItemCount = 0;
-        fromElement.addEventListener("submit", (event) => {
+        this.todoListModel.onChange(() => {
+            const todoListElement = element`<ul />`;
+            const todoItems = this.todoListModel.getTotalItems();
+            todoItems.forEach(item => {
+                const todoItemElement = element`<li>${item.title}</li>`;
+                todoListElement.appendChild(todoItemElement);
+            });
+            render(todoListElement, containerElement);
+            todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+        });
+
+        formElement.addEventListener("submit", (event) => {
             // submitイベントの本来の動作を止める
             event.preventDefault();
-            const todoItemElement = element`<li>${inputElement.value}</li>`;
-            containerElement.appendChild(todoItemElement);
-            todoItemCount += 1;
-            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+
+            this.todoListModel.addTodo(new TodoItemModel({
+                title: inputElement.value,
+                completed: false
+            }));
             inputElement.value = "";
         });
     }
